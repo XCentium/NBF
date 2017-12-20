b7794d17ebf262b4820cf9c086d0622f4f1559ec
Nick Anagnostopoulos <nick.anagnostopoulos@xcentium.com>
2017-12-20 09:42:06 -0600

    adding files

---
+++ src/InsiteCommerce.Web/_SystemResources/Themes/Responsive/Scripts/Cart/insite.cart-lines.directive.ts
@@ -0,0 +1,30 @@
+﻿module insite.cart {
+    "use strict";
+
+    angular
+        .module("insite")
+        .directive("iscCartLines", () => ({
+            restrict: "E",
+            replace: true,
+            templateUrl: "/PartialViews/Cart-CartLines",
+            scope: {
+                cart: "=",
+                promotions: "=",
+                inventoryCheck: "@",
+                includeInventory: "@",
+                includeQuoteRequired: "=",
+                failedToGetRealTimeInventory: "="
+            },
+            controller: "CartLinesController",
+            controllerAs: "vm",
+            link: ($scope: any, element, attrs) => {
+                $scope.editable = attrs.editable === "true";
+                $scope.quoteRequiredFilter = (value) => {
+                    if ($scope.includeQuoteRequired) {
+                        return true;
+                    }
+                    return value.quoteRequired === false;
+                };
+            }
+        }));
+}
