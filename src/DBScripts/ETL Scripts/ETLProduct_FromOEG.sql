389ee0070d4abd3ac2c74e2c95c99314046607b5
Carlos Gonzalez <cgon99>
2018-01-12 17:03:55 -0800

    variant product top level data

--- src/DBScripts/ETL Scripts/ETLProduct_FromOEG.sql
+++ src/DBScripts/ETL Scripts/ETLProduct_FromOEG.sql
@@ -9,7 +9,7 @@ begin
 	declare @brand int
 	set @brand = 1
 
-	--base products per website
+	--base product styles per website
 	
 	insert into StyleClass
 	([Name],[Description],[IsActive],CreatedBy,ModifiedBy)
@@ -24,8 +24,9 @@ begin
 			and spwd.TypeId = 1
 	where 
 		sp.BrandId = @brand
-		and sp.ProductId not in (select [Name] from StyleClass)
+		and convert(nvarchar(max),sp.ProductId) not in (select [Name] from StyleClass)
 
+	--base product info per website
 
 	insert into Product 
 	(ERPNumber, [Name], ERPDescription, ShortDescription,  UrlSegment, ContentManagerId, StyleClassId)
@@ -45,7 +46,7 @@ begin
 		join StyleClass styles on styles.[Name] = sp.ProductId
 	where 
 		sp.BrandId = @brand
-		and sp.ProductId not in (select ERPNumber from Product)
+		and convert(nvarchar(max),sp.ProductId) not in (select ERPNumber from Product)
 	
 
 	update Product set
@@ -74,7 +75,62 @@ begin
 		left join OEGSystemStaging.dbo.LookupUnitOfMeasures luUOM on luUOM.Id = si.UnitOfMeasureId
 		left join OEGSystemStaging.dbo.ItemCartonDimensions scd on scd.ItemId = si.ItemId
 		left join OEGSystemStaging.dbo.LookupItemStatuses luStatus on luStatus.Id = sp.StatusId
-		join Product p on p.ERPNumber = sp.ProductId
+		join Product p on p.ERPNumber = convert(nvarchar(max),sp.ProductId)
+	where 
+		sp.BrandId = @brand
+
+		
+	--variant product info per website
+
+	insert into Product 
+	(ERPNumber, [Name], ERPDescription, ShortDescription,  UrlSegment, ContentManagerId)
+	select  
+		convert(nvarchar(max), sp.ProductId) + '-' + convert(nvarchar(max), spsku.ProductSKUId) ERPNumber,
+		'',
+		ltrim(rtrim(isnull(sisku.[Description],''))) ERPDescription,
+		ltrim(rtrim(isnull(spsku.[Description],''))) ShortDescription,
+		convert(nvarchar(max), sp.ProductId) + '-' + convert(nvarchar(max), spsku.ProductSKUId) UrlSegment,
+		NEWID() ContentManagerId
+	from 
+		OEGSystemStaging.dbo.Products sp
+		join OEGSystemStaging.dbo.Items si on si.ItemId = sp.ItemId
+		join OEGSystemStaging.dbo.ProductSKUs spsku on spsku.ProductId = sp.ProductId
+		join OEGSystemStaging.dbo.ItemSKUs sisku on sisku.ItemSKUId = spsku.ItemSKUId
+	where 
+		sp.BrandId = @brand
+		and convert(nvarchar(max), sp.ProductId) + '-' + convert(nvarchar(max), spsku.ProductSKUId) not in (select ERPNumber from Product)
+
+
+	update Product set
+		ERPDescription = ltrim(rtrim(isnull(sisku.[Description],''))),
+		ShortDescription = ltrim(rtrim(isnull(spsku.[Description],''))),
+		UnitOfMeasure = isnull(luUOM.Code,''),
+		UnitOfMeasureDescription = isnull(luUOM.[Name],''),
+		Sku = isnull(sisku.SKUNumber,''),
+		ShippingWeight = isnull(scd.[Weight],0),
+		ShippingLength = isnull(scd.[Length],0),
+		ShippingWidth = isnull(scd.[Width],0),
+		ShippingHeight = isnull(scd.[Height],0),
+		QtyPerShippingPackage = isnull(si.QtyPerCarton,0),
+		UrlSegment = convert(nvarchar(max), sp.ProductId) + '-' + convert(nvarchar(max), spsku.ProductSKUId),
+		IsDiscontinued = case when spsku.EffEndDate > getdate() then 0 else 1 end,
+		ActivateOn = spsku.EffStartDate,
+		DeactivateOn = spsku.EffEndDate,
+		UPCCode = isnull(sisku.UPCCode,''),
+		ManufacturerItem = spsku.OptionCode,
+		CreatedOn = spsku.CreatedDate,
+		CreatedBy = isnull(spsku.CreatedBy,'etl'),
+		ModifiedOn = spsku.ModifiedDate,
+		ModifiedBy = isnull(spsku.ModifiedBy, 'etl')
+	from 
+		OEGSystemStaging.dbo.Products sp
+		join OEGSystemStaging.dbo.Items si on si.ItemId = sp.ItemId
+		join OEGSystemStaging.dbo.ProductSKUs spsku on spsku.ProductId = sp.ProductId
+		join OEGSystemStaging.dbo.ItemSKUs sisku on sisku.ItemSKUId = spsku.ItemSKUId
+		left join OEGSystemStaging.dbo.LookupUnitOfMeasures luUOM on luUOM.Id = si.UnitOfMeasureId
+		left join OEGSystemStaging.dbo.ItemCartonDimensions scd on scd.ItemId = si.ItemId
+		left join OEGSystemStaging.dbo.LookupItemStatuses luStatus on luStatus.Id = sp.StatusId
+		join Product p on p.ERPNumber = convert(nvarchar(max), sp.ProductId) + '-' + convert(nvarchar(max), spsku.ProductSKUId)
 	where 
 		sp.BrandId = @brand
 
