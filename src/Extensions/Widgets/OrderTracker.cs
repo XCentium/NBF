b68cba554c3336c5bf3e72a267224ed56e90edbd
Dylan <dbarter1@my.wctc.edu>
2018-01-17 16:07:45 -0600

    Order Tracker Widget and controller

---
+++ src/Extensions/Widgets/OrderTracker.cs
@@ -0,0 +1,51 @@
+﻿using Insite.ContentLibrary.ContentFields;
+using Insite.ContentLibrary.Widgets;
+using Insite.Core.Providers;
+using Insite.Data.Entities;
+
+namespace Extensions.Widgets
+{
+    public class OrderTracker : ContentWidget
+    {
+        private string _phoneIsRequiredErrorMessage;
+        private string _phoneIsInvalidErrorMessage;
+        private string _orderNumberIsRequiredErrorMessage;
+        private string _orderNumberIsInvalidErrorMessage;
+        private MessageProvider _messageProvider;
+
+        [RichTextContentField(DisplayName = "Content - Above", IsRequired = true)]
+        public virtual string ContentAbove
+        {
+            get
+            {
+                return GetValue("ContentAbove", "<h3>Look up a single order</h3><p>Enter your order number and phone number to track its status</p>", FieldType.Contextual);
+            }
+            set
+            {
+                SetValue("ContentAbove", value, FieldType.Contextual);
+            }
+        }
+
+        [TextContentField]
+        public virtual string ButtonText
+        {
+            get
+            {
+                return GetValue("ButtonText", "Track Order", FieldType.Contextual);
+            }
+            set
+            {
+                SetValue("ButtonText", value, FieldType.Contextual);
+            }
+        }
+
+        public virtual string PhoneIsRequiredErrorMessage =>
+            _phoneIsRequiredErrorMessage ?? (_phoneIsRequiredErrorMessage = _messageProvider.GetMessage("OrderTracker_PhoneRequired", "Phone Required", ""));
+        public virtual string PhoneIsInvalidErrorMessage =>
+            _phoneIsInvalidErrorMessage ?? (_phoneIsInvalidErrorMessage = _messageProvider.GetMessage("OrderTracker_PhoneInvalid", "Invalid Phone Format", ""));
+        public virtual string OrderNumberIsRequiredErrorMessage =>
+            _orderNumberIsRequiredErrorMessage ?? (_orderNumberIsRequiredErrorMessage = _messageProvider.GetMessage("OrderTracker_OrderNumberRequired", "Order Number Required", ""));
+        public virtual string OrderNumberIsInvalidErrorMessage =>
+            _orderNumberIsInvalidErrorMessage ?? (_orderNumberIsInvalidErrorMessage = _messageProvider.GetMessage("OrderTracker_OrderNumberInvalid", "Invalid Order Number Format", ""));
+    }
+}
---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/OrderTracker/nbf.order-tracker.controller.ts
@@ -0,0 +1,42 @@
+﻿module insite.email {
+    "use strict";
+
+    export class NbfOrderTrackerController {
+        submitted = false;
+        $form: JQuery;
+
+        static $inject = ["$element", "$scope"];
+
+        constructor(
+            protected $element: ng.IRootElementService,
+            protected $scope: ng.IScope) {
+            this.init();
+        }
+
+        init(): void {
+            this.$form = this.$element.find("form");
+            this.$form.removeData("validator");
+            this.$form.removeData("unobtrusiveValidation");
+            $.validator.unobtrusive.parse(this.$form);
+        }
+
+        submit($event): boolean {
+            $event.preventDefault();
+            if (!this.$form.valid()) {
+                return false;
+            }
+
+            (this.$form as any).ajaxPost(() => {
+                this.submitted = true;
+                this.$scope.$apply();
+                alert('submitted');
+            });
+
+            return false;
+        }
+    }
+
+    angular
+        .module("insite")
+        .controller("NbfOrderTrackerController", NbfOrderTrackerController);
+}
