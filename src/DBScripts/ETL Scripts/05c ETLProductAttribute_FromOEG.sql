3f4e62bd2dd6625dc9bc8ce98e116b165b0a7a58
Carlos Gonzalez <cgon99>
2018-01-22 20:07:48 -0800

    colors attribute, dimensions specification and code clean up

--- src/DBScripts/ETL Scripts/05c ETLProductAttribute_FromOEG.sql
+++ src/DBScripts/ETL Scripts/05c ETLProductAttribute_FromOEG.sql
@@ -10,9 +10,41 @@ begin
 	exec dbo.IsDataReady  'OEGSystem Snapshot', @IsReady output
 	if @IsReady = 0	return;
 
+	declare @brand int
+	set @brand = 1
+
 	declare @attributeName nvarchar(255)
 	declare @attributeTypeId uniqueidentifier
 
+
+	set @attributeName = 'Colors'
+	exec ETLAttribute_FromOEG @attributeName
+	select top 1 @attributeTypeId = Id from AttributeType where [Name] = @attributeName
+
+	
+	-- since there is no "key" on this table it's tough to do an "update", we are just going to add new ones and manually delete unused ones (if needed)
+	insert into AttributeValue (AttributeTypeId, [Value], SortOrder, IsActive, CreatedBy, ModifiedBy)
+	select distinct @attributeTypeId, slu.[Name], 0, 1, 'etl', 'etl'
+	from OEGSystemStaging.dbo.LookupWebColors slu
+	where slu.[Name] not in (select [Value] from AttributeValue where AttributeTypeId = @attributeTypeId)
+
+
+	-- since there is no primary key here, we can just delete all associations and repopulate
+	delete from ProductAttributeValue
+	where AttributeValueId in ( select avalue.Id from AttributeValue avalue
+	join AttributeType atype on atype.Id = avalue.AttributeTypeId and atype.Id = @attributeTypeId)
+
+	
+	insert into ProductAttributeValue (ProductId, AttributeValueId) 
+	select distinct p.Id, avalue.Id 
+	from Product p
+	join OEGSystemStaging.dbo.Products sp on sp.Number = p.ERPNumber
+		and sp.BrandId = @brand
+	join OEGSystemStaging.dbo.ProductSKUs spsku on spsku.ProductId = sp.ProductId
+	join OEGSystemStaging.dbo.ItemSKUsColors sd on sd.ItemSKUId = spsku.ItemSKUId
+	join OEGSystemStaging.dbo.LookupWebColors slu on slu.ColorId = sd.ColorId
+	join AttributeValue avalue on avalue.[Value] = slu.[Name]
+	where avalue.AttributeTypeId = @attributeTypeId
 
 	set @attributeName = 'Materials'
 	exec ETLAttribute_FromOEG @attributeName
@@ -36,16 +68,40 @@ begin
 	select distinct p.Id, avalue.Id 
 	from Product p
 	join OEGSystemStaging.dbo.Products sp on sp.Number = p.ERPNumber
+		and sp.BrandId = @brand
 	join OEGSystemStaging.dbo.ItemsMaterials sd on sd.ItemId = sp.ItemId
 	join OEGSystemStaging.dbo.LookupItemMaterials slu on slu.MaterialId = sd.MaterialId
 	join AttributeValue avalue on avalue.[Value] = slu.[Name]
 	where avalue.AttributeTypeId = @attributeTypeId
 	
 
-	--- TODO
 	set @attributeName = 'Styles'
 	exec ETLAttribute_FromOEG @attributeName
 	select top 1 @attributeTypeId = Id from AttributeType where [Name] = @attributeName
+
+	-- since there is no "key" on this table it's tough to do an "update", we are just going to add new ones and manually delete unused ones (if needed)
+	insert into AttributeValue (AttributeTypeId, [Value], SortOrder, IsActive, CreatedBy, ModifiedBy)
+	select distinct @attributeTypeId, slu.DisplayName, 0, 1, 'etl', 'etl'
+	from OEGSystemStaging.dbo.ItemStyleDisplayNames slu
+	where slu.DisplayName not in (select [Value] from AttributeValue where AttributeTypeId = @attributeTypeId)
+		and slu.BrandId = @brand
+
+
+	-- since there is no primary key here, we can just delete all associations and repopulate
+	delete from ProductAttributeValue
+	where AttributeValueId in ( select avalue.Id from AttributeValue avalue
+	join AttributeType atype on atype.Id = avalue.AttributeTypeId and atype.Id = @attributeTypeId)
+
+	
+	insert into ProductAttributeValue (ProductId, AttributeValueId) 
+	select distinct p.Id, avalue.Id 
+	from Product p
+	join OEGSystemStaging.dbo.Products sp on sp.Number = p.ERPNumber
+		and sp.BrandId = @brand
+	join OEGSystemStaging.dbo.ItemsStyles sd on sd.ItemId = sp.ItemId
+	join OEGSystemStaging.dbo.ItemStyleDisplayNames slu on slu.StyleId = sd.StyleId
+	join AttributeValue avalue on avalue.[Value] = slu.DisplayName
+	where avalue.AttributeTypeId = @attributeTypeId
 
 
 
@@ -70,6 +126,7 @@ begin
 	select distinct p.Id, avalue.Id 
 	from Product p
 	join OEGSystemStaging.dbo.Products sp on sp.Number = p.ERPNumber
+		and sp.BrandId = @brand
 	join OEGSystemStaging.dbo.ItemsRooms sd on sd.ItemId = sp.ItemId
 	join OEGSystemStaging.dbo.LookupItemRooms slu on slu.RoomId = sd.RoomId
 	join AttributeValue avalue on avalue.[Value] = slu.[Name]
