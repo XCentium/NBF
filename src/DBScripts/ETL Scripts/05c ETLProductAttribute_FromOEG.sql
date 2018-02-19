40b6f1be39fb55a9422487f0d1191fcad2ea859f
Carlos Gonzalez <cgon99>
2018-02-19 15:35:14 -0800

    "green" attribute

--- src/DBScripts/ETL Scripts/05c ETLProductAttribute_FromOEG.sql
+++ src/DBScripts/ETL Scripts/05c ETLProductAttribute_FromOEG.sql
@@ -484,6 +484,45 @@ begin
 
 
 
+
+
+	set @attributeName = 'Green'
+	exec ETLAttribute_FromOEG @attributeName
+	select top 1 @attributeTypeId = Id from AttributeType where [Name] = @attributeName
+
+	-- since there is no "key" on this table it's tough to do an "update", we are just going to add new ones and manually delete unused ones (if needed)
+	if not exists (select 1 from AttributeValue where AttributeTypeId = @attributeTypeId and [value] = 'Yes')
+	begin
+		insert into AttributeValue (AttributeTypeId, [Value], SortOrder, IsActive, CreatedBy, ModifiedBy)
+		select @attributeTypeId, 'Yes', 0, 1, 'etl', 'etl'
+	end
+	if not exists (select 1 from AttributeValue where AttributeTypeId = @attributeTypeId and [value] = 'No')
+	begin
+		insert into AttributeValue (AttributeTypeId, [Value], SortOrder, IsActive, CreatedBy, ModifiedBy)
+		select @attributeTypeId, 'No', 0, 1, 'etl', 'etl'
+	end
+
+
+	-- since there is no primary key here, we can just delete all associations and repopulate
+	delete from ProductAttributeValue
+	where AttributeValueId in ( select avalue.Id from AttributeValue avalue
+	join AttributeType atype on atype.Id = avalue.AttributeTypeId and atype.Id = @attributeTypeId)
+
+	
+	insert into ProductAttributeValue (ProductId, AttributeValueId) 
+	select distinct p.Id, avalue.Id 
+	from Product p
+	join OEGSystemStaging.dbo.Products sp on sp.Number = p.ERPNumber
+		and sp.BrandId = 1
+	left join OEGSystemStaging.dbo.ItemsAttributeValues sd on sd.ItemId = sp.ItemId
+	left join OEGSystemStaging.dbo.LookupItemAttributeValues slu on slu.AttributeValueId = sd.AttributeValueId
+		and slu.[Name] in ('Greenguard','Greenguard Child/School')
+	left join OEGSystemStaging.dbo.LookupItemAttributes sluName on sluName.AttributeId = slu.AttributeId
+		and sluName.[Name] = 'Eco Friendly' 
+	join AttributeValue avalue on avalue.[Value] = case when sd.AttributeValueId is not null then 'Yes' else 'No' end
+	where avalue.AttributeTypeId = @attributeTypeId
+
+
 /*
 
 exec ETLProductAttribute_FromOEG
