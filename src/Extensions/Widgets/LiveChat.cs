3e9d8703de414d22929ff2759f144325e62dd662
Dylan <dbarter1@my.wctc.edu>
2018-02-21 12:28:39 -0600

    Header Widgets

---
+++ src/Extensions/Widgets/LiveChat.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using Insite.ContentLibrary.Widgets;
+
+namespace Extensions.Widgets
+{
+    [DisplayName("NBF - Live Chat")]
+    public class LiveChat : ContentWidget
+    {
+    }
+}
