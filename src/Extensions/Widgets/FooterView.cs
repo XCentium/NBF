494bf529e4f524b27b792328127387906f2c1f05
Arvinder Chopra <andychopra@bancroftway.com>
2018-02-21 10:00:17 -0800

    Link list with Title

--- src/Extensions/Widgets/FooterView.cs
+++ src/Extensions/Widgets/FooterView.cs
@@ -1,9 +1,13 @@
 ﻿using Insite.ContentLibrary.ContentFields;
+using Insite.ContentLibrary.Pages;
 using Insite.ContentLibrary.Widgets;
 using Insite.Data.Entities;
+using Insite.WebFramework.Content.Attributes;
+using System;
 
 namespace Extensions.Widgets
 {
+    [AllowedParents(new Type[] { typeof(Footer) })]
     public class FooterView : ContentWidget
     {
     }
