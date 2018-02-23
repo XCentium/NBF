f387f74a7d943c728ffa8bd2bfa6fbce31699eb1
Dylan <dbarter1@my.wctc.edu>
2018-02-23 13:32:14 -0600

    Fix footerview class

--- src/Extensions/Widgets/FooterView.cs
+++ src/Extensions/Widgets/FooterView.cs
@@ -1,9 +1,11 @@
 using System.ComponentModel;
+using Insite.ContentLibrary.Pages;
 using Insite.ContentLibrary.Widgets;
+using Insite.WebFramework.Content.Attributes;
 
 namespace Extensions.Widgets
 {
-    [AllowedParents(new Type[] { typeof(Footer) })]
+    [AllowedParents(typeof(Footer))]
     [DisplayName("NBF - Footer View")]
 
     public class FooterView : ContentWidget
