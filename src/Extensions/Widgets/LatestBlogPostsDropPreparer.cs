5188c681a1d58135e4495ae8941819d3bd161574
Arvinder Chopra <andychopra@bancroftway.com>
2018-02-27 21:48:40 -0800

    Latest Blog posts widget

---
+++ src/Extensions/Widgets/LatestBlogPostsDropPreparer.cs
@@ -0,0 +1,74 @@
+﻿using Insite.ContentLibrary.Pages;
+using Insite.ContentLibrary.Widgets;
+using Insite.Core.Interfaces.Data;
+using Insite.Core.Interfaces.Localization;
+using Insite.Data.Entities;
+using Insite.WebFramework;
+using Insite.WebFramework.Content;
+using Insite.WebFramework.Content.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Extensions.Widgets
+{
+    public class LatestBlogPostsDropPreparer : GenericPreparer<LatestBlogPosts>
+    {
+        protected readonly IContentHelper ContentHelper;
+        protected readonly HttpContextBase HttpContext;
+        protected readonly IUnitOfWork UnitOfWork;
+        
+        public LatestBlogPostsDropPreparer(IContentHelper contentHelper, HttpContextBase httpContext, ITranslationLocalizer translationLocalizer, IUnitOfWorkFactory unitOfWorkFactory)
+          : base(translationLocalizer)
+        {
+            this.UnitOfWork = unitOfWorkFactory.GetUnitOfWork();
+            this.ContentHelper = contentHelper;
+            this.HttpContext = httpContext;
+        }
+
+        public override void Prepare(LatestBlogPosts contentItem)
+        {
+            LatestBlogPostsDrop viewModel = this.CreateViewModel();
+            this.PopulateViewModel(viewModel, contentItem);
+            contentItem.Drop = viewModel;
+        }
+
+        protected virtual LatestBlogPostsDrop CreateViewModel()
+        {
+            return new LatestBlogPostsDrop();
+        }
+
+        protected virtual void PopulateViewModel(LatestBlogPostsDrop model, LatestBlogPosts articleList)
+        {
+            var tagSet = new HashSet<string>();
+            
+            var contentKeyList = this.ContentHelper.GetChildPages<NewsPage>(articleList.PageContentKey, true)
+                .OrderByDescending<NewsPage, DateTimeOffset?>((Func<NewsPage, DateTimeOffset?>)(o => o.PublishDate))
+                .ToList<NewsPage>();
+
+            var pageLinkDropList = new List<LatestBlogPostDrop>();
+            int counter = 0;
+            foreach (int page in articleList.BlogPosts)
+            {
+                counter++;
+                GetPageResult<AbstractPage> pageByVariantKey = PageContext.Current.ContentHelper.GetPageByVariantKey(page, false);
+                if (pageByVariantKey.Page != null && !pageByVariantKey.Page.IsRetracted
+                    && pageByVariantKey.DisplayLink)
+                {
+                    var blogPage = pageByVariantKey.Page as NewsPage;
+                    pageLinkDropList.Add(new LatestBlogPostDrop()
+                    {
+                        Url = PageContext.Current.GenerateUrl(blogPage),
+                        Title = blogPage.Title,
+                        Summary = blogPage.Summary,
+                        PublishDate = blogPage.PublishDate.Value.UtcDateTime.ToShortDateString(),
+                        Author = blogPage.Author,
+                        Number = counter
+                    });
+                }
+            }
+            model.PageLinks = pageLinkDropList;
+        }
+    }
+}
