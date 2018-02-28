b9722d7aaa72bc79df955eb5d9635160f17af6d2
Dylan <dbarter1@my.wctc.edu>
2018-02-28 15:19:32 -0600

    OT Updates to work with 4.4 upgrade

---
+++ src/Extensions/Widgets/OrderTrackerPreparer.cs
@@ -0,0 +1,22 @@
+﻿using Extensions.Settings;
+using Insite.Core.Interfaces.Localization;
+using Insite.WebFramework.Content;
+
+namespace Extensions.Widgets
+{
+    public class OrderTrackerPreparer : GenericPreparer<OrderTracker>
+    {
+        protected readonly OrderTrackerSettings OrderTrackerSettings;
+
+        public OrderTrackerPreparer(ITranslationLocalizer translationLocalizer, OrderTrackerSettings orderTrackerSettings)
+            : base(translationLocalizer)
+        {
+            OrderTrackerSettings = orderTrackerSettings;
+        }
+
+        public override void Prepare(OrderTracker contentItem)
+        {
+            contentItem.OrderTrackerDetailsUrl = OrderTrackerSettings.OrderTrackerDetailUrl;
+        }
+    }
+}
