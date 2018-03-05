ae710e3ef363450fdd09be316144feb9f83eb076
Arvinder Chopra <andychopra@bancroftway.com>
2018-03-05 15:00:53 -0800

    Added Featured Products widget

---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Catalog/nbf.featuredproducts-widget.controller.ts
@@ -0,0 +1,50 @@
+﻿module insite.catalog {
+    "use strict";
+
+    export interface IFeaturedProductsWidgetControllerAttributes extends ng.IAttributes {
+        productIdString: string;
+    }
+
+    export class FeaturedProductsWidgetController {
+        productIds: string[];
+        products: ProductModel[]; 
+
+        static $inject = ["$timeout", "$window", "$scope", "$rootScope", "$attrs", "productService", "sessionService"];
+
+        constructor(
+            protected $timeout: ng.ITimeoutService,
+            protected $window: ng.IWindowService,
+            protected $scope: ng.IScope,
+            protected $rootScope: ng.IRootScopeService,
+            protected $attrs: IFeaturedProductsWidgetControllerAttributes,
+            protected productService: IProductService,
+            protected sessionService: account.ISessionService) {
+            this.init();
+        }
+
+        init(): void {
+            this.productIds = this.$attrs.productIdString.split(":");
+            this.getProducts();
+        }
+
+        protected getProducts(): void {
+            const expand = ["pricing", "attributes"];
+            this.products = [];
+            this.productIds.forEach(id => {
+                this.productService.getProduct(null, id, expand).then(
+                    (x: any) => {
+                        if (x && x.product) {
+                            this.products.push(x.product);
+                        }
+                    },
+                    (error: any) => {
+                        console.error(error);
+                    });
+            });
+        }
+    }
+
+    angular
+        .module("insite")
+        .controller("FeaturedProductsWidgetController", FeaturedProductsWidgetController);
+}
