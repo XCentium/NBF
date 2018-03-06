cfadd6f5a7490c1c2d123eaa0279814017d55707
Dylan <dbarter1@my.wctc.edu>
2018-03-06 09:41:05 -0600

    potential fix for latestblogposts

--- src/Extensions/Widgets/LatestBlogPostsDropPreparer.cs
+++ src/Extensions/Widgets/LatestBlogPostsDropPreparer.cs
@@ -1,12 +1,9 @@
 ﻿using Insite.ContentLibrary.Pages;
-using Insite.ContentLibrary.Widgets;
 using Insite.Core.Interfaces.Data;
 using Insite.Core.Interfaces.Localization;
-using Insite.Data.Entities;
 using Insite.WebFramework;
 using Insite.WebFramework.Content;
 using Insite.WebFramework.Content.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -43,27 +40,30 @@ namespace Extensions.Widgets
         {           
             var pageLinkDropList = new List<LatestBlogPostDrop>();
             int counter = 0;
-            foreach (int page in articleList.BlogPosts)
+            foreach (var page in articleList.BlogPosts)
             {
                 counter++;
-                GetPageResult<AbstractPage> pageByVariantKey = this.ContentHelper.GetPageByVariantKey(page, false);
-                if (pageByVariantKey.Page != null && !pageByVariantKey.Page.IsRetracted
-                    && pageByVariantKey.DisplayLink)
+                GetPageResult<AbstractPage> pageByContentKey = this.ContentHelper.GetPage(page);
+                if (pageByContentKey.Page != null && !pageByContentKey.Page.IsRetracted
+                    && pageByContentKey.DisplayLink)
                 {
-                    var blogPage = pageByVariantKey.Page as NewsPage;
-                    var articlePageView = ContentHelper.GetWidgets(blogPage.ContentKey, "Content")
-                        .FirstOrDefault(x => x is ArticlePageView) as ArticlePageView;
-
-                    pageLinkDropList.Add(new LatestBlogPostDrop()
+                    var blogPage = pageByContentKey.Page as NewsPage;
+                    if (blogPage != null)
                     {
-                        Url = PageContext.Current.GenerateUrl(blogPage),
-                        Title = blogPage.Title,
-                        Summary = blogPage.Summary,
-                        PublishDate = blogPage.PublishDate.Value.UtcDateTime.ToShortDateString(),
-                        Author = blogPage.Author,
-                        Number = counter,
-                        BackgroundImageUrl = articlePageView != null ? articlePageView.ArticleImageUrl : string.Empty
-                    });
+                        var articlePageView = ContentHelper.GetWidgets(blogPage.ContentKey, "Content")
+                            .FirstOrDefault(x => x is ArticlePageView) as ArticlePageView;
+
+                        pageLinkDropList.Add(new LatestBlogPostDrop()
+                        {
+                            Url = PageContext.Current.GenerateUrl(blogPage),
+                            Title = blogPage.Title,
+                            Summary = blogPage.Summary,
+                            PublishDate = blogPage.PublishDate?.UtcDateTime.ToShortDateString(),
+                            Author = blogPage.Author,
+                            Number = counter,
+                            BackgroundImageUrl = articlePageView != null ? articlePageView.ArticleImageUrl : string.Empty
+                        });
+                    }
                 }
             }
             model.PageLinks = pageLinkDropList;
