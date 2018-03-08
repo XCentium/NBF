2f926a39fe7056a2c0df28b18defc3c5b7013870
Dylan <dbarter1@my.wctc.edu>
2018-03-08 08:46:41 -0600

    updates

---
+++ src/Extensions/Widgets/CategoriesListView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Insite.ContentLibrary.ContentFields;
+using Insite.ContentLibrary.Widgets;
+using Microsoft.Ajax.Utilities;
+using FieldType = Insite.Data.Entities.FieldType;
+
+namespace Extensions.Widgets
+{
+    [DisplayName("NBF - Category List View")]
+    public class CategorieslistView : ContentWidget
+    {
+        [TextContentField(IsRequired = true)]
+        public virtual string RootCategoryId
+        {
+            get
+            {
+                return GetValue(nameof(RootCategoryId), "", FieldType.Contextual);
+            }
+            set
+            {
+                SetValue(nameof(RootCategoryId), value, FieldType.Contextual);
+            }
+        }
+
+        public virtual bool CategoryIdSet => !RootCategoryId.IsNullOrWhiteSpace();
+    }   
+}
