213b828c54926da8079ca0b90b70006aca1af0db
Dylan <dbarter1@my.wctc.edu>
2018-03-13 16:46:57 -0500

    cta edit

--- src/Extensions/Widgets/CTALinkZone.cs
+++ src/Extensions/Widgets/CTALinkZone.cs
@@ -1,11 +1,9 @@
 ﻿using System.ComponentModel;
 using Insite.ContentLibrary.Widgets;
-using Insite.WebFramework.Content.Attributes;
 
 namespace Extensions.Widgets
 {
     [DisplayName("NBF - CTA Link Zone")]
-    [AllowedChildren(typeof(CtaLinkWidget))]
     public class CtaLinkZone : ContentWidget
     {
 
