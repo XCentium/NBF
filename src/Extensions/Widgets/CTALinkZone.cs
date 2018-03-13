f3375016ace0eadd68dcf5025def829ee35d0c5b
Dylan <dbarter1@my.wctc.edu>
2018-03-13 16:45:32 -0500

    Call out for corporate accounts

---
+++ src/Extensions/Widgets/CTALinkZone.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Insite.ContentLibrary.Widgets;
+using Insite.WebFramework.Content.Attributes;
+
+namespace Extensions.Widgets
+{
+    [DisplayName("NBF - CTA Link Zone")]
+    [AllowedChildren(typeof(CtaLinkWidget))]
+    public class CtaLinkZone : ContentWidget
+    {
+
+    }
+}
