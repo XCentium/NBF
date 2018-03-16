f627d9362d7b45a66f4e550fdd8bdd709308d564
Carlos Gonzalez <cgon99>
2018-03-16 13:11:09 -0700

    delivery info queries

---
+++ src/DBScripts/Test Scripts/delivery info v01.sql
@@ -0,0 +1,20 @@
+select ProductId,DeliveryAmount,ItemId from products where number = '44179'
+select RequireTruck from items where ItemId = 2543
+
+select * from ProductSKUs where ProductId = 82670
+select top 1000 ShipTypeId,ShipCodeId,NormalLeadTimeId, * from ItemSKUs where ItemId = 2543
+
+select * from LookupLeadtimes
+select * from LookupShipTypes
+select * from LookupShipCodes
+
+--ShipTypeId = non-truck = 'N' 
+--DeliveryAmount = 12.30
+--RequireTruck = null
+--OptionCount = 5
+
+if optioncount = 1 
+Allow 2-5 business days for delivery. 
+
+html table
+
