ab3d1be1e59018314f4f0a5d1494beb00fafe74d
Nick Anagnostopoulos <nick.anagnostopoulos@xcentium.com>
2018-03-15 19:09:52 -0500

    adding zoom, video, and 360 to product detail page

---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Catalog/nbf.product-images.controller.ts
@@ -0,0 +1,58 @@
+﻿module insite.catalog {
+    "use strict";
+
+    export class NBFProductImagesController extends ProductImagesController {
+
+
+        init(): void {
+            this.$scope.$watch(() => this.product.productImages, () => {
+                if (this.product.productImages.length > 0) {
+                    this.selectedImage = this.product.productImages[0];
+                } else {
+                    this.selectedImage = {
+                        smallImagePath: this.product.smallImagePath,
+                        mediumImagePath: this.product.mediumImagePath,
+                        largeImagePath: this.product.largeImagePath,
+                        altText: this.product.altText
+                    } as ProductImageDto;
+                }
+                scene7InitWith(this.selectedImage.smallImagePath);
+            }, true);
+            
+            (angular.element("#imgZoom") as any).foundation("reveal");
+
+            angular.element(document).on("close.fndtn.reveal", "#imgZoom[data-reveal]:visible", () => { this.onImgZoomClose(); });
+
+            angular.element(document).on("opened.fndtn", "#imgZoom[data-reveal]", () => { this.onImgZoomOpened(); });
+        }
+
+        protected onImgZoomClose(): void {
+            this.$scope.$apply(() => {
+                this.showCarouselOnZoomModal = false;
+            });
+        }
+
+        protected onImgZoomOpened(): void {
+            this.$scope.$apply(() => {
+                this.showCarouselOnZoomModal = true;
+            });
+        }
+
+        getMainImageWidth(): number {
+            return angular.element(`#${this.mainPrefix}ProductImage`).outerWidth();
+        }
+
+        getZoomImageWidth(): number {
+            return angular.element(`#${this.zoomPrefix}ProductImage`).outerWidth();
+        }
+    }
+
+    angular
+        .module("insite")
+        .filter('trusted', ['$sce', function ($sce) {
+            return function (url) {
+                return $sce.trustAsResourceUrl(url);
+            };
+        }])
+        .controller("ProductImagesController", NBFProductImagesController);
+}
