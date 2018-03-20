4981fc537f64b2ded58181e27f33f179ca696021
MARKMONDRAGON-X\Mark.Mondragon-XC <mark.mondragon@xcentium.com>
2018-03-20 14:45:18 -0700

    873 resource landing filters for product list page.

---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Dealers/nbf.dealer-list.controller.ts
@@ -0,0 +1,21 @@
+﻿module insite.dealers {
+    "use strict";
+
+    export class NbfDealerCollectionController extends DealerCollectionController {
+       
+        protected setDealersMarkers(): void {
+            this.dealers.forEach((dealer, i) => {
+                const marker = this.createMarker(dealer.latitude, dealer.longitude, `<span class='ico ico-Pin loc-marker'><span class="number">${this.getDealerNumber(i)}</span></span>`);
+
+                google.maps.event.addListener(marker, "click", () => {
+                    this.onDealerMarkerClick(marker, dealer);
+                });
+            });
+        }
+        
+    }
+
+    angular
+        .module("insite")
+        .controller("DealerCollectionController", NbfDealerCollectionController);
+}
