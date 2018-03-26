06adfce0c1608d57f8e09be943e534eb28db0267
Dylan <dbarter1@my.wctc.edu>
2018-03-26 12:05:35 -0500

    Article List updates

--- src/Extensions/Widgets/ArticleListViewPreparer.cs
+++ src/Extensions/Widgets/ArticleListViewPreparer.cs
@@ -23,12 +23,12 @@ namespace Extensions.Widgets
         public ArticleListViewPreparer(IContentHelper contentHelper, HttpContextBase httpContext, ITranslationLocalizer translationLocalizer ,IUnitOfWorkFactory unitOfWorkFactory)
           : base(translationLocalizer)
         {
-            this.UnitOfWork = unitOfWorkFactory.GetUnitOfWork();
+            UnitOfWork = unitOfWorkFactory.GetUnitOfWork();
 
             var tagSet = new HashSet<string>();
-            var tagField = this.UnitOfWork.GetRepository<ContentItem>().GetTable().Where(x => x.IsDeleted == false && x.IsRetracted == false && x.PublishOn != null)
-                .Join(this.UnitOfWork.GetRepository<ContentItemField>().GetTable(), ci => ci.ContentKey, cif => cif.ContentKey,
-                    (ci, cif) => new { ci = ci, cif = cif })
+            var tagField = UnitOfWork.GetRepository<ContentItem>().GetTable().Where(x => x.IsDeleted == false && x.IsRetracted == false && x.PublishOn != null)
+                .Join(UnitOfWork.GetRepository<ContentItemField>().GetTable(), ci => ci.ContentKey, cif => cif.ContentKey,
+                    (ci, cif) => new {ci, cif })
                     .Where(x => x.cif.FieldName == "Css" || x.cif.FieldName == "Url");
             foreach(var tag in tagField)
             {
@@ -38,14 +38,14 @@ namespace Extensions.Widgets
                 }
                 
             }
-            this.ContentHelper = contentHelper;
-            this.HttpContext = httpContext;
+            ContentHelper = contentHelper;
+            HttpContext = httpContext;
         }
 
         public override void Prepare(ArticleListView contentItem)
         {
-            ArticleListViewDrop viewModel = this.CreateViewModel();
-            this.PopulateViewModel(viewModel, contentItem);
+            ArticleListViewDrop viewModel = CreateViewModel();
+            PopulateViewModel(viewModel, contentItem);
             contentItem.Drop = viewModel;
         }
 
@@ -57,69 +57,62 @@ namespace Extensions.Widgets
         protected virtual void PopulateViewModel(ArticleListViewDrop model, ArticleListView articleList)
         {
             string str1 = articleList.Id.ToString();
-            int intFromQueryString1 = this.HttpContext.Request.ParseIntFromQueryString(string.Format("{0}_page", (object)str1), 1);
-            string pageFilter = this.HttpContext.Request.QueryString.Get("tag");
+            int intFromQueryString1 = HttpContext.Request.ParseIntFromQueryString($"{str1}_page", 1);
+            string pageFilter = HttpContext.Request.QueryString.Get("tag");
 
-            int intFromQueryString2 = this.HttpContext.Request.ParseIntFromQueryString(string.Format("{0}_pageSize", (object)str1), articleList.DefaultPageSize);
-            List<NewsPage> list = new List<NewsPage>();
+            int intFromQueryString2 = HttpContext.Request.ParseIntFromQueryString($"{str1}_pageSize", articleList.DefaultPageSize);
 
-            list = this.ContentHelper.GetChildPages<NewsPage>(articleList.PageContentKey, true).OrderByDescending<NewsPage, DateTimeOffset?>((Func<NewsPage, DateTimeOffset?>)(o => o.PublishDate)).ToList<NewsPage>();
+            var list = ContentHelper.GetChildPages<NewsPage>(articleList.PageContentKey).OrderByDescending(o => o.PublishDate).ToList();
             var filteredHash = new HashSet<NewsPage>();
             if (pageFilter != null) {
                 foreach (var item in list)
                 {
-                    var tagField = this.UnitOfWork.GetRepository<ContentItem>().GetTable().Where(x => x.ParentKey == item.ContentKey && x.IsDeleted == false && x.IsRetracted == false && x.PublishOn != null)
-                        .Join(this.UnitOfWork.GetRepository<ContentItemField>().GetTable(), ci => ci.ContentKey, cif => cif.ContentKey,
-                            (ci, cif) => new { ci = ci, cif = cif })
+                    var tagField = UnitOfWork.GetRepository<ContentItem>().GetTable().Where(x => x.ParentKey == item.ContentKey && x.IsDeleted == false && x.IsRetracted == false && x.PublishOn != null)
+                        .Join(UnitOfWork.GetRepository<ContentItemField>().GetTable(), ci => ci.ContentKey, cif => cif.ContentKey,
+                            (ci, cif) => new {ci, cif })
                             .Where(x => x.cif.FieldName == "PageTags");
 
                     foreach (var tag in tagField)
                     {
-                        if (tag.cif.ObjectValue != null && tag.cif.ObjectValue.Count() > 0)
+                        if (tag.cif.ObjectValue != null && tag.cif.ObjectValue.Any())
                         {
                             var oValue = tag.cif.ObjectValue.ToObject() as List<string>;
-                            if (oValue.Contains(pageFilter))
+                            if (oValue != null && oValue.Contains(pageFilter))
                             {
                                 filteredHash.Add(item);
                             }
 
                         }
                     }
-                    //var pageFields = this.ContentHelper.GetWidgets(item.ContentKey, "Content").DefaultIfEmpty(); //.Where(z => z.Class == "PageTagSelectView");
-                    //foreach (var p in pageFields)
-                    //{
-                    //    foreach (var field in p.CurrentContentItemFields)
-                    //    {
-                    //        if (field.Key == "PageTags")
-                    //        {
-                    //            if (((List<string>)field.Value.ObjectValue).Count > 0)
-                    //            {
-                    //                filteredList.Add(item);
-                    //            }
-                    //        }
-                    //    }
-                    //}
-                    ////.OrderByDescending<NewsPage, DateTimeOffset?>((Func<NewsPage, DateTimeOffset?>)(o => o.PublishDate)).ToList<NewsPage>();
                 }
                 list = filteredHash.ToList();
             }
-            if (!list.Any<NewsPage>())
+            if (!list.Any())
                 return;
             model.NewsListId = str1;
-            model.NewsPages = (ICollection<ArticleListViewPageDrop>)list.Skip<NewsPage>(intFromQueryString2 * (intFromQueryString1 - 1)).Take<NewsPage>(intFromQueryString2).Select<NewsPage, ArticleListViewPageDrop>((Func<NewsPage, ArticleListViewPageDrop>)(p =>
+            model.NewsPages = list.Skip(intFromQueryString2 * (intFromQueryString1 - 1)).Take(intFromQueryString2).Select(p =>
             {
-                ArticleListViewPageDrop listViewPageDrop = new ArticleListViewPageDrop();
-                listViewPageDrop.Url = PageContext.Current.GenerateUrl((AbstractPage)p);
-                listViewPageDrop.Title = p.Title;
-                listViewPageDrop.Author = p.Author;
+                var articlePageView = ContentHelper.GetWidgets(p.ContentKey, "Content")
+                    .FirstOrDefault(x => x is ArticlePageView) as ArticlePageView;
+
+                ArticleListViewPageDrop listViewPageDrop =
+                    new ArticleListViewPageDrop
+                    {
+                        Url = PageContext.Current.GenerateUrl(p),
+                        Title = p.Title,
+                        Author = p.Author,
+                        QuickSummaryDisplay = p.Summary,
+                        Image = articlePageView != null ? articlePageView.ArticleImageUrl : string.Empty,
+                        ImageFocalPosition = articlePageView != null ? articlePageView.ArticleImageWidgetViewFocalPosition : string.Empty
+                    };
                 DateTimeOffset? publishDate = p.PublishDate;
 
-                string str2 = ((publishDate).HasValue ? (publishDate).GetValueOrDefault().LocalDateTime.ToShortDateString() : (string)null) ?? string.Empty;
+                string str2 = ((publishDate).HasValue ? (publishDate).GetValueOrDefault().LocalDateTime.ToShortDateString() : null) ?? string.Empty;
                 listViewPageDrop.PublishDateDisplay = str2;
                 string str3 = p.Summary.IsBlank() ? p.NewsContents.Substring(0, p.NewsContents.Length > 500 ? 500 : p.NewsContents.Length) + "..." : p.Summary;
                 listViewPageDrop.QuickSummaryDisplay = str3;
                 return listViewPageDrop;
-            })).ToList<ArticleListViewPageDrop>();
+            }).ToList();
             model.Pagination = new PagingInfo(intFromQueryString1, intFromQueryString2, list.Count, articleList.DefaultPageSize);
         }
     }
