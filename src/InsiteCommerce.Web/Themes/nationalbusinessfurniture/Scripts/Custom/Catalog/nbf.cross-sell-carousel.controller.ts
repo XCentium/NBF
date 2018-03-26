8c4dea4adcb9a90da7d9d38309f32a124f3b0064
Dylan <dbarter1@my.wctc.edu>
2018-03-26 18:22:27 -0500

    Prod Carousel

---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Catalog/nbf.cross-sell-carousel.controller.ts
@@ -0,0 +1,47 @@
+﻿module insite.catalog {
+    "use strict";
+
+    export class NbfCrossSellCarouselController extends CrossSellCarouselController {
+
+        protected initializeCarousel(): void {
+            $(".cs-carousel").flexslider({
+                animation: "slide",
+                controlNav: false,
+                animationLoop: true,
+                slideshow: false,
+                itemWidth: this.getItemSize(),
+                minItems: this.getItemsNumber(),
+                maxItems: this.getItemsNumber(),
+                itemMargin: 36,
+                move: this.getItemsMove(),
+                controlsContainer: $(".custom-controls-container"),
+                customDirectionNav: $(".carousel-control-nav"),
+                start: (slider: any) => { this.onCarouselStart(slider); }
+            });
+
+            $(window).resize(() => { this.onWindowResize(); });
+        }
+
+        protected isAttributeValue(product: ProductDto, attrName: string, attrValue: string): boolean {
+            let retVal = false;
+
+            if (product && product.attributeTypes) {
+                const attrType = product.attributeTypes.find(x => x.name === attrName && x.isActive === true);
+
+                if (attrType) {
+                    const matchingAttrValue = attrType.attributeValues.find(y => y.value === attrValue);
+
+                    if (matchingAttrValue) {
+                        retVal = true;
+                    }
+                }
+            }
+            return retVal;
+        }
+
+    }
+
+    angular
+        .module("insite")
+        .controller("CrossSellCarouselController", NbfCrossSellCarouselController);
+}
