3ed45cc80711497f8c45ec6fd3375479c92bd887
Nick Anagnostopoulos <nick.anagnostopoulos@xcentium.com>
2018-03-28 16:48:58 -0500

    adding api for split payments

---
+++ src/Extensions/WebApi/CreditCardTransaction/Controllers/NBFCCTransactionController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Insite.Core.Interfaces.Data;
+using Insite.Core.Plugins.Utilities;
+using Insite.Core.WebApi;
+using Insite.Data.Entities;
+using Extensions.WebApi.Messages.Interfaces;
+using Extensions.WebApi.Messages.Models;
+using Extensions.WebApi.CreditCardTransaction.Models;
+using Extensions.WebApi.CreditCardTransaction.Interfaces;
+
+namespace Extensions.WebApi.CreditCardTransaction.Controllers
+{
+    [RoutePrefix("api/nbf/creditcardtransaction")]
+    public class NBFCCTransactionController : BaseApiController
+    {
+        private readonly ICCTransactionService _ccService;
+
+        public NBFCCTransactionController(ICookieManager cookieManager, ICCTransactionService ccService)
+          : base(cookieManager)
+        {
+            _ccService = ccService;
+        }
+
+        [Route("AddCCTransaction", Name = "AddCCTransaction"), HttpPost]
+        [ResponseType(typeof(string))]
+        public string CreateMessage(AddCCTransactionParameter parameter)
+        {
+            var a = this._ccService.AddCCTransaction(parameter);
+
+            return a.ToString();
+        }
+    }
+}
---
+++ src/Extensions/WebApi/CreditCardTransaction/Interfaces/ICCTransactionService.cs
@@ -0,0 +1,13 @@
+﻿using Extensions.WebApi.CreditCardTransaction.Models;
+using Extensions.WebApi.Messages.Models;
+using Insite.Core.Interfaces.Dependency;
+using Insite.Order.Services.Results;
+using System.Threading.Tasks;
+
+namespace Extensions.WebApi.CreditCardTransaction.Interfaces
+{
+    public interface ICCTransactionService : IDependency, IExtension
+    {
+        bool AddCCTransaction(AddCCTransactionParameter parameter);
+    }
+}
