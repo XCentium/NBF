6d3c8e865bebd5544df2b2dc84f7ab43718790e8
DJ Sitte <djs@nbf.com>
2018-04-12 13:45:55 -0500

    Created start to analytics

---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Analytics/nbf.analytics.datalayer.ts
@@ -0,0 +1,117 @@
+﻿module nbf.analytics {
+
+    export class AnalyticsDataLayer {
+        pageInfo: AnalyticsPageInfo = new AnalyticsPageInfo();
+        cart: AnalyticsCart = new AnalyticsCart();
+        product: AnalyticsProduct = new AnalyticsProduct();
+        transaction: AnalyticsTransaction = new AnalyticsTransaction();
+        profile: AnalyticsProfile = new AnalyticsProfile();
+    }
+
+
+    export class AnalyticsCart {
+        cartID: string;
+        price: AnalyticsCartPrice = new AnalyticsCartPrice();
+        items: AnalyticsCartItem[] = [];
+    }
+
+    export class AnalyticsCartPrice {
+        basePrice: number;
+        promoCode: string;
+        promoDiscount: number;
+        bulkDiscount: number;
+        totalDiscount: number;
+        tax: number;
+        estimatedShipping: number;
+        estimatedTotal: number;
+    }
+
+    export class AnalyticsCartItem {
+        productName: string;
+        sku: string;
+        description: string;
+        productImage: string;
+        vendor: string;
+        collection: string;
+        category: string;
+        basePrice: number;
+        promoDiscount: number;
+        bulkDiscount: number;
+        totalDiscount: number;
+        finalPrice: number;
+    }
+
+    export class AnalyticsProduct {
+        productInfo: AnalyticsProductInfo = new AnalyticsProductInfo();
+        relatedProductsInfo: AnalyticsProductInfo[] = [];
+    }
+
+    export class AnalyticsProductInfo {
+        productName: string;
+        sku: string;
+        description: string;
+        productImage: string;
+        vendor: string;
+        collection: string;
+        category: string;
+        basePrice: number;
+        salePrice: number;
+    }
+
+    export class AnalyticsPageInfo {
+        //Must be unique across all pages
+        pageName: string;
+        //Page template being used (i.e. Product Page)
+        pageType: string;
+        siteSection: string;
+        siteSubsection: string;
+        destinationUrl: string;
+        referringUrl: string;
+        breadCrumbs: string;
+        isErrorPage: boolean;
+        transId: string;
+        internalSearch: AnalyticsPageSearchInfo;
+    }
+
+    export class AnalyticsPageSearchInfo {
+        searchTerm: string;
+        searchResults: number;
+    }
+
+    export class AnalyticsTransaction {
+        transactionId: string;
+        shippingAddress: AnalyticsAddress = new AnalyticsAddress();
+        billingAddress: AnalyticsAddress = new AnalyticsAddress();
+        paymentMethod: string;
+        total: AnalyticsTransactionTotal = new AnalyticsTransactionTotal();
+    }
+
+    export class AnalyticsTransactionTotal {
+        basePrice: number;
+        promoCode: string;
+        promoDiscount: number;
+        bulkDiscount: number;
+        tax: number;
+        shipping: number;
+        transactionTotal: number;
+    }
+
+    export class AnalyticsAddress {
+        line1: string;
+        line2: string;
+        city: string;
+        stateProvince: string;
+        postalCode: string;
+        country: string;
+    }
+
+    export class AnalyticsProfile {
+        isAuthenticated: boolean;
+        profileInfo: AnalyticsProfileInfo = new AnalyticsProfileInfo();
+    }
+
+    export class AnalyticsProfileInfo {
+        profileId: string;
+        email: string;
+    }
+}
---
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Analytics/nbf.analytics.service.ts
@@ -0,0 +1,173 @@
+﻿import ICartService = insite.cart.ICartService;
+
+module nbf.analytics {
+
+    export class AdobeAnalyticsService {
+
+        get Data(): AnalyticsDataLayer {
+            if (window["digitalData"]) {
+                window["digitalData"] = new AnalyticsDataLayer();
+            }
+            return window["digitalData"];
+        }
+        set Data(dataLayer: AnalyticsDataLayer) {
+            window["digitalData"] = dataLayer;
+        }
+
+        get PageInfo(): AnalyticsPageInfo {
+            return this.Data.pageInfo;
+        }
+        set PageInfo(pageInfo: AnalyticsPageInfo) {
+            this.Data.pageInfo = pageInfo;
+        }
+
+        get Cart(): AnalyticsCart {
+            return this.Data.cart;
+        }
+        set Cart(cart: AnalyticsCart) {
+            this.Data.cart = cart;
+        }
+
+        get Product() {
+            return this.Data.product;
+        }
+        set Product(product: AnalyticsProduct) {
+            this.Data.product = product;
+        }
+
+        get Transaction() {
+            return this.Data.transaction;
+        }
+        set Transaction(transaction: AnalyticsTransaction) {
+            this.Data.transaction = transaction;
+        }
+
+        get Profile() {
+            return this.Data.profile;
+        }
+        set Profile(profile: AnalyticsProfile) {
+            this.Data.profile = profile;
+        }     
+
+        public PageLoad(): void {
+            this.FireEvent("PageLoad");
+        }
+
+        public ViewProductPage(): void {
+            this.FireEvent("ProductView");
+        }
+
+        public SwatchRequest(): void {
+            this.FireEvent("SwatchRequest");
+        }
+
+        public CatalogRequest(): void {
+            this.FireEvent("CatalogRequest");
+        }
+
+        public QuoteRequest(): void {
+            this.FireEvent("QuoteRequest");
+        }
+
+        public MiniCartQuoteRequest(): void {
+            this.FireEvent("MiniCartQuoteRequest");
+        }
+
+        public InternalSearch(): void {
+            this.FireEvent("InternalSearch");
+        }
+
+        public SuccessfulSearch(): void {
+            this.FireEvent("SuccessfulSearch");
+        }
+
+        public FailedSearch(): void {
+            this.FireEvent("FailedSearch");
+        }
+
+        public ContactUsInitiated(): void {
+            this.FireEvent("ContactUsInitiated");
+        }
+
+        public ContactUsCompleted(): void {
+            this.FireEvent("ContactUsCompleted");
+        }
+
+        public AccountCreation(): void {
+            this.FireEvent("AccountCreation");
+        }
+
+        public CheckoutAccountCreation(): void {
+            this.FireEvent("CheckoutAccountCreation");
+        }
+
+        public Login(): void {
+            this.FireEvent("Login");
+        }
+
+        public CrossSellSelected(): void {
+            this.FireEvent("CrossSellSelected");
+        }
+
+        public EmailSignUp(): void {
+            this.FireEvent("EmailSignUp");
+        }
+
+        public LiveVideoChatStarted(): void {
+            this.FireEvent("LiveVideoChatStarted");
+        }
+
+        public LiveTextChatStarted(): void {
+            this.FireEvent("LiveTextChatStarted");
+        }
+
+        public ProductAddedToCart(): void {
+            this.FireEvent("ProductAddedToCart");
+        }
+
+        public CheckoutInitiated(): void {
+            this.FireEvent("CheckoutInitiated");
+        }
+
+        public ProductQuestionAsked(): void {
+            this.FireEvent("ProductQuestionAsked");
+        }
+
+        public Selected360View(): void {
+            this.FireEvent("Selected360View");
+        }
+
+        public AddProductToWishlist(): void {
+            this.FireEvent("AddProductToWishlist");
+        }
+
+        public SaveOrderFromCartPage(): void {
+            this.FireEvent("SaveOrderFromCartPage");
+        }
+
+        public ContinueShoppingFromCartPage(): void {
+            this.FireEvent("ContinueShoppingFromCartPage");
+        }
+
+        public ReadReviewsSelected(): void {
+            this.FireEvent("ReadReviewsSelected");
+        }
+
+        public MiniCartHover(): void {
+            this.FireEvent("MiniCartHover");
+        }
+
+        public SaveCart(): void {
+            this.FireEvent("SaveCart");
+        }
+
+        private FireEvent(event: string) {
+            console.log("Firing event: " + event);
+            console.log(this.Data);
+        }
+
+
+
+    }
+
+}
