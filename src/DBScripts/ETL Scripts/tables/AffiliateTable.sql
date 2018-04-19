2995cfb3131b3a7f51e1ea26306c2ff3cb2a4f01
Carlos Gonzalez <cgon99>
2018-04-19 13:33:28 -0700

    updates name

--- src/DBScripts/ETL Scripts/tables/AffiliateTable.sql
+++ src/DBScripts/ETL Scripts/tables/AffiliateTable.sql
@@ -1,13 +1,12 @@
-/****** Object:  Table [extensions].[AffiliateCode]    Script Date: 4/18/2018 10:01:47 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [extensions].[AffiliateCode](
+CREATE TABLE [Extensions].[AffiliateCode](
 	[Id] [uniqueidentifier] NOT NULL,
-	[AffiliateID] [int] NOT NULL,
+	[AffiliateNumber] [int] NOT NULL,
 	[AffiliateCode] [nvarchar](100) NOT NULL,
 	[CreatedOn] [datetimeoffset](7) NULL,
 	[CreatedBy] [nvarchar](100) NOT NULL,
@@ -20,26 +19,26 @@ CREATE TABLE [extensions].[AffiliateCode](
 ) ON [PRIMARY]
 GO
 
-/****** Object:  Index [IX_AffiliateCode_NaturalKey]    Script Date: 4/18/2018 10:01:47 AM ******/
-CREATE UNIQUE NONCLUSTERED INDEX [IX_AffiliateCode_NaturalKey] ON [extensions].[AffiliateCode]
+/****** Object:  Index [IX_AffiliateNumber_NaturalKey]    Script Date: 4/19/2018 1:32:14 PM ******/
+CREATE UNIQUE NONCLUSTERED INDEX [IX_AffiliateNumber_NaturalKey] ON [Extensions].[AffiliateCode]
 (
-	[AffiliateID] ASC
+	[AffiliateNumber] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 GO
 
-ALTER TABLE [extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_Id]  DEFAULT (newsequentialid()) FOR [Id]
+ALTER TABLE [Extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_Id]  DEFAULT (newsequentialid()) FOR [Id]
 GO
 
-ALTER TABLE [extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_CreatedOn]  DEFAULT (getutcdate()) FOR [CreatedOn]
+ALTER TABLE [Extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_CreatedOn]  DEFAULT (getutcdate()) FOR [CreatedOn]
 GO
 
-ALTER TABLE [extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_CreatedBy]  DEFAULT ('') FOR [CreatedBy]
+ALTER TABLE [Extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_CreatedBy]  DEFAULT ('') FOR [CreatedBy]
 GO
 
-ALTER TABLE [extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_ModifiedOn]  DEFAULT (getutcdate()) FOR [ModifiedOn]
+ALTER TABLE [Extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_ModifiedOn]  DEFAULT (getutcdate()) FOR [ModifiedOn]
 GO
 
-ALTER TABLE [extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_ModifiedBy]  DEFAULT ('') FOR [ModifiedBy]
+ALTER TABLE [Extensions].[AffiliateCode] ADD  CONSTRAINT [DF_AffiliateCode_ModifiedBy]  DEFAULT ('') FOR [ModifiedBy]
 GO
 
 
