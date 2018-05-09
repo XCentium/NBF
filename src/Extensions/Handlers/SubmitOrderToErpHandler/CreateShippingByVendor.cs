2eb9ec18fc91906b017788a2deda2bb53fe61546
DJ Sitte <djs@nbf.com>
2018-05-09 13:15:00 -0500

    Updating shipping calculations to breakout shipping charges by vendor

---
+++ src/Extensions/Handlers/SubmitOrderToErpHandler/CreateShippingByVendor.cs
@@ -0,0 +1,45 @@
+﻿using Insite.Cart.Services.Parameters;
+using Insite.Cart.Services.Results;
+using Insite.Core.Services.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Insite.Core.Interfaces.Data;
+using Extensions.Utility.Shipping;
+using Extensions.Models.ShippingChargesRule;
+using Extensions.Models.ShippingByVendor;
+using Insite.Core.Interfaces.Dependency;
+
+namespace Extensions.Handlers.SubmitOrderToErpHandler
+{
+    [DependencyName(nameof(CreateShippingByVendor))]
+    public sealed class CreateShippingByVendor : HandlerBase<UpdateCartParameter, UpdateCartResult>
+    {
+        public override int Order => 2999;
+
+        public override UpdateCartResult Execute(IUnitOfWork unitOfWork, UpdateCartParameter parameter, UpdateCartResult result)
+        {
+            if (parameter.Status.EqualsIgnoreCase("submitted"))
+            {
+                var additionalCharges = unitOfWork.GetRepository<ShippingChargesRuleModel>().GetTable().ToList();
+                var repo = unitOfWork.GetRepository<ShippingByVendorModel>();
+
+                var shipByVendor = ShippingHelper.CalculateShippingByVendor(additionalCharges, result.GetCartResult.Cart);
+
+                foreach(var vendor in shipByVendor)
+                {
+                    repo.Insert(new ShippingByVendorModel()
+                    {
+                        OrderNumber = result.GetCartResult.Cart.OrderNumber,
+                        ShippingCost = vendor.ShippingCost,
+                        VendorId = vendor.VendorId,
+                        ShipCode = vendor.ShipCode                        
+                    });
+                }
+            }
+
+            return NextHandler.Execute(unitOfWork, parameter, result);
+        }
+    }
+}
---
+++ src/Extensions/Models/ShippingByVendor/ShippingByVendorModel.cs
@@ -0,0 +1,27 @@
+﻿using Insite.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Extensions.Models.ShippingByVendor
+{
+    [Table("ShippingByVendor", Schema = "Extensions")]
+    public class ShippingByVendorModel : EntityBase
+    {
+        [Required]
+        public string OrderNumber { get; set; }
+        
+        [Required]
+        public Guid VendorId { get; set; }
+
+        [Required]
+        public decimal ShippingCost { get; set; }
+
+        [Required]
+        public string ShipCode { get; set; }
+
+    }
+}
