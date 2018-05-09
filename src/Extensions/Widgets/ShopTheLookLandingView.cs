77c7cd8dab15ac3429c35325dcce862f9807b653
Dylan <dbarter1@my.wctc.edu>
2018-05-09 10:45:10 -0500

    STL Updates

--- src/Extensions/Widgets/ShopTheLookLandingView.cs
+++ src/Extensions/Widgets/ShopTheLookLandingView.cs
@@ -1,10 +1,24 @@
 ﻿using System.ComponentModel;
+using Insite.ContentLibrary.ContentFields;
 using Insite.ContentLibrary.Widgets;
+using Insite.Data.Entities;
 
 namespace Extensions.Widgets
 {
     [DisplayName("NBF - Shop The Look Landing Page View")]
     public class ShopTheLookLandingView : ContentWidget
     {
+        [RichTextContentField(IsRequired = true)]
+        public virtual string NoResultsContent
+        {
+            get
+            {
+                return GetValue("NoResultsContent", "No Looks Found.", FieldType.Contextual);
+            }
+            set
+            {
+                SetValue("NoResultsContent", value, FieldType.Contextual);
+            }
+        }
     }
 }
