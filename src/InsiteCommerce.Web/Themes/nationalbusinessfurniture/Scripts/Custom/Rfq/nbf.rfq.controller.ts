dc83108ddf75bcce58aacd0bd7cc4f1001dab794
DJ Sitte <djs@nbf.com>
2018-05-30 13:38:08 -0500

    Removed debugger calls

--- src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Rfq/nbf.rfq.controller.ts
+++ src/InsiteCommerce.Web/Themes/nationalbusinessfurniture/Scripts/Custom/Rfq/nbf.rfq.controller.ts
@@ -2,24 +2,7 @@
     "use strict";
 
     export class NbfRfqController extends insite.rfq.RfqController{
-        static $inject = ["coreService", "$scope", "cartService", "rfqService", "accountService", "sessionService", "settingsService", "$q"];
-
-        constructor(
-            protected coreService: insite.core.ICoreService,
-            protected $scope: ng.IScope,
-            protected cartService: insite.cart.ICartService,
-            protected rfqService: insite.rfq.IRfqService,
-            protected accountService: insite.account.IAccountService,
-            protected sessionService: insite.account.ISessionService,
-            protected settingsService: insite.core.ISettingsService,
-            protected $q: ng.IQService) {
-            super(coreService, $scope, cartService, rfqService, accountService, sessionService, settingsService, $q);
-            debugger;
-            this.init();
-        }
-
         protected submitQuoteCompleted(quote: QuoteModel, successUri): void {
-            debugger;
             this.$scope.$root.$broadcast("AnalyticsEvent", "QuoteRequest");
             super.submitQuoteCompleted(quote, successUri);
         }
