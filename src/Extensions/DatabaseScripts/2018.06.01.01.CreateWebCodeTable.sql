fe8fead49b65b98fe05c85950786576c04690752
kjohnson <kjohnson@VMPC-KEITHJ>
2018-06-04 08:23:44 -0500

    Update DB Script

--- src/Extensions/DatabaseScripts/2018.06.01.01.CreateWebCodeTable.sql
+++ src/Extensions/DatabaseScripts/2018.06.01.01.CreateWebCodeTable.sql
@@ -5,6 +5,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+DROP TABLE [Extensions].[WebcodeUniqueID]
+GO
+
 CREATE TABLE [Extensions].[WebcodeUniqueID](
     [Id] [uniqueidentifier] NOT NULL CONSTRAINT [DF_WebcodeUniqueID_Id]  DEFAULT (newsequentialid()),
     [WebcodeUniqueID] [bigint] IDENTITY(1,1) NOT NULL,
@@ -18,4 +21,17 @@ CONSTRAINT [PK_WebcodeUniqueID] PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = ON, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 80) ON [PRIMARY]
 ) ON [PRIMARY]
 
+GO
+/****** Create Referral Link for Testing  *******/
+DELETE FROM [Extensions].[AffiliateCode] 
+      WHERE [Extensions].[AffiliateCode].AffiliateCode = 'ochdev'
+GO
+
+INSERT INTO [Extensions].[AffiliateCode]
+           (
+           [AffiliateNumber]
+           ,[AffiliateCode]
+           )
+     VALUES
+           ('14439200','ochdev')
 GO
