764ba09a1f1eaa25d75bc7bbb39b8eacdae98106
DJ Sitte <djs@nbf.com>
2018-06-26 13:48:55 -0500

    Creating static navigation

---
+++ src/Extensions/Models/StaticCategory/StaticCategoryMapping.cs
@@ -0,0 +1,19 @@
+﻿using Insite.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace Extensions.Models.StaticCategory
+{
+    public class StaticCategoryMapping : EntityTypeConfiguration<StaticCategory>, ICommerceContextMapping
+    {
+        public StaticCategoryMapping()
+        {
+            HasMany(e => e.CustomProperties)
+            .WithOptional()
+            .HasForeignKey(e => e.ParentId);
+        }
+    }
+}
